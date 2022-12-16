38043356a6797da6f7c1183df8a282dd1ccf000f
joedonovan <joepeterdonovan@gmail.com>
2022-12-15 19:51:38 -0600

    third_commits

--- code.js
+++ code.js
@@ -119,6 +119,8 @@ const scores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 // let playerTwo = 26; 
 
 const deck = new Deck(); 
+const playerOne = deck.deal().playerOne; 
+const playerTwo = deck.deal().playerTwo; 
 deck.createDeck(suits, ranks, scores); 
 //deck.shuffle(); 
 //console.log(deck.deal()); 
@@ -128,27 +130,54 @@ deck.createDeck(suits, ranks, scores);
 
 let playerOneScore = 26; 
 let playerTwoScore = 26; 
-const compare = array => {
-        let winner = "error message"; 
-        if (array[0].score > array[1].score){
-        winner = "Player One wins!";
-        playerOneScore ++; 
-        playerTwoScore --
-    }else if (array[0].score < array[1].score){
-        winner = "Player Two wins!"
-        playerOneScore -- ; 
-        playerTwoScore ++ ; 
+const compare = (playerOne, playerTwo) => {
+        let i = 0
+        let winner = "error"; 
+        while(playerOneScore > 0 && playerTwoScore > 0){
+        if(deck.deal().playerOne[i].score > deck.deal().playerTwo[i].score){
+            winner = 'player one wins!'
+            playerOneScore ++; 
+            playerTwoScore -- ;
+            i ++
+            break; 
+        } else if(deck.deal().playerTwo[i].score > deck.deal().playerOne[i].score){
+            winner = 'player two wins!'
+            playerOneScore --; 
+            playerTwoScore ++;
+            i++
+            break; 
         }
         return (`${winner} 
-        Player One played ${deck.deal()[0].rank} of ${deck.deal()[0].suit}
-        Player Two played ${deck.deal()[1].rank} of ${deck.deal()[1].suit}
+        Player One played ${playerOne[i].rank} of ${playerOne[i].suit}
+        Player Two played ${playerTwo[i].rank} of ${playerTwo[i].suit}
         
         ***CARDS REMAINING***
         player One: ${playerOneScore}
         player Two: ${playerTwoScore}`);
-    }    
-// console.log(compare(myArray)); 
+            
+            }
+        }
+     
+//         if (playerOne[].score > array[1].score){
+//         winner = "Player One wins!";
+//         playerOneScore ++; 
+//         playerTwoScore --
+//     }else if (array[0].score < array[1].score){
+//         winner = "Player Two wins!"
+//         playerOneScore -- ; 
+//         playerTwoScore ++ ; 
+//         }
+//         return (`${winner} 
+//         Player One played ${deck.deal()[0].rank} of ${deck.deal()[0].suit}
+//         Player Two played ${deck.deal()[1].rank} of ${deck.deal()[1].suit}
+        
+//         ***CARDS REMAINING***
+//         player One: ${playerOneScore}
+//         player Two: ${playerTwoScore}`);
+//     }    
+// // console.log(compare(myArray)); 
 // deck.deal(); 
 //console.log(deck);
 deck.shuffle(); 
 console.log(deck.deal());
+console.log(compare(deck.deal().playerOne, deck.deal().playerTwo)); 
